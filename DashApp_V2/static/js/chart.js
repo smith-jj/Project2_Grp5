917f681f9e63cfa21ab7e13333aeaccb0878bbb5
smith-jj <50722440+smith-jj@users.noreply.github.com>
2019-10-12 15:22:37 -0500

    created new

--- DashApp_V2/static/js/chart.js
+++ DashApp_V2/static/js/chart.js
@@ -1,113 +1,45 @@
-$(document).ready(function() {
+// Global parameters:
+// do not resize the chart canvas when its container does (keep at 600x400px)
+Chart.defaults.global.responsive = false;
 
-    var TITLE = '$th Grade Test Scores';
+// define the chart data
+var chartData = {
+    labels: [{ % for item in labels % }
+        "{{item}}",
+        { % endfor % }
+    ],
+    datasets: [{
+        label: '{{ legend }}',
+        fill: true,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [{ % for item in values % } {
+                { item } },
+            { % endfor % }
+        ],
+        spanGaps: false
+    }]
+}
 
-    // Which column names contain data points?
-    var X_LABELS = ['2009', '2011', '2013', '2015', '2017'];
+// get chart canvas
+var ctx = document.getElementById("myChart").getContext("2d");
 
-    // Which column name contains names (labels) for each series?
-    var NAMES = 'Scores';
-
-    // Optionally, which column contains color names for the series?
-    // If not specified, will apply default color scheme
-    var COLORS = 'color';
-
-    var X_AXIS = 'Academic Year'; // x-axis label and label in tooltip
-    var Y_AXIS = 'Test Scores'; // y-axis label and label in tooltip
-
-    var SHOW_GRID = true; // `true` to show the grid, `false` to hide
-    var SHOW_LEGEND = true; // `true` to show the legend, `false` to hide
-
-    // Read data file and create a chart
-    d3.csv('data/NationScores.csv').then(function(rows) {
-
-        var datasets = rows.map(function(row) {
-
-            var dataset = {
-                label: row[NAMES],
-                fill: false,
-                data: X_LABELS.map(function(t) { return row[t] })
-            }
-
-            if (row[COLORS]) {
-                dataset.backgroundColor = row[COLORS];
-                dataset.borderColor = row[COLORS];
-                dataset.pointBackgroundColor = row[COLORS];
-                dataset.pointBorderColor = row[COLORS];
-            }
-
-            return dataset;
-
-        });
-
-        var lineChartData = {
-            labels: X_LABELS,
-            datasets: datasets
-        };
-
-        var ctx = document.getElementById('container').getContext('2d');
-
-        new Chart(ctx, {
-            type: 'line',
-            data: lineChartData,
-
-            options: {
-                title: {
-                    display: true,
-                    text: TITLE,
-                    fontSize: 14,
-                },
-                legend: {
-                    display: SHOW_LEGEND,
-                },
-                scales: {
-                    xAxes: [{
-                        scaleLabel: {
-                            display: X_AXIS !== '',
-                            labelString: X_AXIS
-                        },
-                        gridLines: {
-                            display: SHOW_GRID,
-                        },
-                        ticks: {
-                            callback: function(value, index, values) {
-                                return value.toLocaleString();
-                            }
-                        }
-                    }],
-                    yAxes: [{
-                        beginAtZero: true,
-                        scaleLabel: {
-                            display: Y_AXIS !== '',
-                            labelString: Y_AXIS
-                        },
-                        gridLines: {
-                            display: SHOW_GRID,
-                        },
-                        ticks: {
-                            callback: function(value, index, values) {
-                                return value.toLocaleString()
-                            }
-                        }
-                    }]
-                },
-                tooltips: {
-                    displayColors: false,
-                    callbacks: {
-                        label: function(tooltipItem, all) {
-                            return all.datasets[tooltipItem.datasetIndex].label +
-                                ': ' + tooltipItem.yLabel.toLocaleString();
-                        }
-                    }
-                },
-                plugins: {
-                    colorschemes: {
-                        scheme: 'brewer.Paired12'
-                    }
-                }
-            }
-        });
-
-    });
-
+// create the chart using the chart canvas
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: chartData,
 });
