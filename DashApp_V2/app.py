0e9566d0121de0b215713a854f18d0b54eaa283e
smith-jj <50722440+smith-jj@users.noreply.github.com>
2019-10-12 13:44:39 -0500

    WORKING FLASK app.py

--- DashApp_V2/app.py
+++ DashApp_V2/app.py
@@ -32,12 +32,10 @@ male_reading = Base.classes.male_reading
 female_reading = Base.classes.female_reading
 
 
-
 @app.route("/")
 def index():
     """Return the homepage."""
     return render_template("index.html")
-
 
 
 @app.route("/state")
@@ -48,10 +46,8 @@ def state():
     df = pd.read_sql_query(stmt, db.session.bind)
     state_list = list(df.state)
 
-
     # Return a list of the column states
     return jsonify(state_list)
-
 
 
 @app.route("/boymath/<state>")
@@ -64,6 +60,7 @@ def boy_math_scores(state):
         male_math.state,
         male_math.avg_2009_mathScores,
         male_math.avg_2017_mathScores,
+        male_math.mathScores_precentChange,
     ]
 
     results = db.session.query(
@@ -75,35 +72,11 @@ def boy_math_scores(state):
         boy_math_scores["state"] = result[0]
         boy_math_scores["avg_2009_mathScores"] = result[1]
         boy_math_scores["avg_2017_mathScores"] = result[2]
+        boy_math_scores["mathScores_precentChange"] = result[3]
 
    # Return Jsonified data ()
     print(boy_math_scores)
     return jsonify(boy_math_scores)
-
-
-@app.route("/boymathgap")
-def boymath_gap():
-    """Return Gap in Boy's Math Scores"""
-
-    # perform the sql query for math test scores comparison
-    # avg_2009_math, avg_2017_math, avg_math_perchg
-    sel = [
-        male_math.state,
-        male_math.mathScores_precentChange,
-    ]
-
-    results = db.session.query(
-        *sel).all()
-
-    # Create a dictionary entry for each row of math data information
-    boymath_gap = {}
-    for result in results:
-        boymath_gap["state"] = result[0]
-        boymath_gap["mathScores_precentChange"] = result[1]
-
-   # Return Jsonified data ()
-    print(boymath_gap)
-    return jsonify(boymath_gap)
 
 
 @app.route("/girlmath/<state>")
@@ -116,6 +89,7 @@ def girl_math_scores(state):
         female_math.state,
         female_math.avg_2009_mathScores,
         female_math.avg_2017_mathScores,
+        female_math.mathScores_precentChange,
     ]
 
     results = db.session.query(
@@ -127,35 +101,12 @@ def girl_math_scores(state):
         girl_math_scores["state"] = result[0]
         girl_math_scores["avg_2009_mathScores"] = result[1]
         girl_math_scores["avg_2017_mathScores"] = result[2]
+        girl_math_scores["mathScores_precentChange"] = result[3]
 
    # Return Jsonified data ()
     print(girl_math_scores)
     return jsonify(girl_math_scores)
 
-
-@app.route("/girlmathgap")
-def girlmath_gap():
-    """Return Gap in Girls's Math Scores"""
-
-    # perform the sql query for math test scores comparison
-    # avg_2009_math, avg_2017_math, avg_math_perchg
-    sel = [
-        female_math.state,
-        female_math.mathScores_precentChange,
-    ]
-
-    results = db.session.query(
-        *sel).all()
-
-    # Create a dictionary entry for each row of math data information
-    girlmath_gap = {}
-    for result in results:
-        girlmath_gap["state"] = result[0]
-        girlmath_gap["mathScores_precentChange"] = result[1]
-
-   # Return Jsonified data ()
-    print(girlmath_gap)
-    return jsonify(girlmath_gap)
 
 @app.route("/boyreading/<state>")
 def boy_reading_scores(state):
@@ -167,6 +118,7 @@ def boy_reading_scores(state):
         male_reading.state,
         male_reading.avg_2009_readingScores,
         male_reading.avg_2017_readingScores,
+        male_reading.readingScores_precentChange,
     ]
 
     results = db.session.query(
@@ -178,35 +130,11 @@ def boy_reading_scores(state):
         boy_reading_scores["state"] = result[0]
         boy_reading_scores["avg_2009_readingScores"] = result[1]
         boy_reading_scores["avg_2017_readingScores"] = result[2]
+        boy_reading_scores["readingScores_precentChange"] = result[3]
 
    # Return Jsonified data ()
     print(boy_reading_scores)
     return jsonify(boy_reading_scores)
-
-
-@app.route("/boyreadinggap")
-def boyreading_gap():
-    """Return Gap in Boy's Reading Scores"""
-
-    # perform the sql query for reading test scores comparison
-    # avg_2009_reading, avg_2017_reading, avg_reading_perchg
-    sel = [
-        male_reading.state,
-        male_reading.readingScores_precentChange,
-    ]
-
-    results = db.session.query(
-        *sel).all()
-
-    # Create a dictionary entry for each row of math data information
-    boyreading_gap = {}
-    for result in results:
-        boyreading_gap["state"] = result[0]
-        boyreading_gap["readingScores_precentChange"] = result[1]
-
-   # Return Jsonified data ()
-    print(boyreading_gap)
-    return jsonify(boyreading_gap)
 
 
 @app.route("/girlreading/<state>")
@@ -219,6 +147,7 @@ def girl_reading_scores(state):
         female_reading.state,
         female_reading.avg_2009_readingScores,
         female_reading.avg_2017_readingScores,
+        female_reading.readingScores_precentChange,
     ]
 
     results = db.session.query(
@@ -230,35 +159,12 @@ def girl_reading_scores(state):
         girl_reading_scores["state"] = result[0]
         girl_reading_scores["avg_2009_readingScores"] = result[1]
         girl_reading_scores["avg_2017_readingScores"] = result[2]
+        girl_reading_scores["readingScores_precentChange"] = result[3]
 
    # Return Jsonified data ()
     print(girl_reading_scores)
     return jsonify(girl_reading_scores)
 
-
-@app.route("/girlreadinggap")
-def girlreading_gap():
-    """Return Gap in Girls's Reading Scores"""
-
-    # perform the sql query for reading test scores comparison
-    # avg_2009_reading, avg_2017_reading, avg_reading_perchg
-    sel = [
-        female_reading.state,
-        female_reading.readingScores_precentChange,
-    ]
-
-    results = db.session.query(
-        *sel).all()
-
-    # Create a dictionary entry for each row of math data information
-    girlreading_gap = {}
-    for result in results:
-        girlreading_gap["state"] = result[0]
-        girlreading_gap["readingScores_precentChange"] = result[1]
-
-   # Return Jsonified data ()
-    print(girlreading_gap)
-    return jsonify(girlreading_gap)
 
 if __name__ == "__main__":
     app.run(debug=True)
